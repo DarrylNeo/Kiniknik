3b40c782d8714ad35768688f7fe6b68f01531fbf
Frankloct <frank.lingzi98@gmail.com>
2022-10-11 00:22:27 +1100

    wk10

---
+++ Wk10/FL/DetectPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+
+    [SerializeField] Material alarm;
+    [SerializeField] Material allclear;
+    MeshRenderer renderer;
+    Sensor sensor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        sensor = GetComponentInChildren<Sensor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sensor.isDetecting)
+        {
+            renderer.material = alarm;
+        } else
+        {
+            renderer.material = allclear;
+        }
+    }
+}
